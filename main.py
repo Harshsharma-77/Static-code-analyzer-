6e153819e1456af14a3051be5d56845a2a5e28a5
Harshsharma-77 <harshsharma7194@gmail.com>
2026-04-24 09:59:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -74,5 +74,7 @@ async def analyze_code(req: AnalyzeRequest):
     }
 
 if __name__ == "__main__":
-    print("Starting Compiler Backend...")
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
